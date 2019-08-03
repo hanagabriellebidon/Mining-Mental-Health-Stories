003c8be7db0aa4cc92f5ffec7c6c55e34ef2e642
Hana Gabrielle Bidon <37670365+hrb56@users.noreply.github.com>
2019-08-03 05:43:30 -0400

    Add files via upload

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,9 +1,11 @@
 import nltk
 from nltk.tokenize import sent_tokenize
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import sys
 
-mental_health_story = open('mentalhealth_hana_gabrielle.txt', 'r').read()
-sentences = sent_tokenize(mental_health_story)
+# Mental Health Stories from Echoes of Silence
+mental_health_story_1 = open('mentalhealth_hana_gabrielle.txt', 'r').read()
+sentences = sent_tokenize(mental_health_story_1)
 
 sid = SentimentIntensityAnalyzer()
 for sentence in sentences:
@@ -12,29 +14,3 @@ for sentence in sentences:
     for k in ss:
         print('{0}: {1}, '.format(k, ss[k]), end='')
     print()
-#     for k in ss:
-#         print(‘{0}: {1}, ‘.format(k, ss[k]), end=’’)
-#     print()
-
-# Remove punctuation from sentences.
-# def remove_punctuation(sentence):
-#     sentence = re.sub(r'[^\w\s]','', sentence)
-#     return sentence
-# cleaned_sent = [remove_punctuation(sentence) for sentence in sentences]
-# partial_story = cleaned_sent
-#
-# partial_story_words = [word_tokenize(sentence) for sentence in partial_story]
-# print(partial_story_words)
-#
-# stop_words = list(set(stopwords.words('english')))
-# print('Number of stopwords:', len(stop_words))
-# print(f'Stop words:\n{stop_words}')
-#
-# def remove_stopword(sentence):
-#     return [w for w in sentence if not w in stop_words]
-#
-# filtered = [remove_stopword(s) for s in partial_story_words]
-# print(filtered)
-#
-# POS = [nltk.pos_tag(tokenized_sent) for tokenized_sent in filtered]
-# print(POS)
